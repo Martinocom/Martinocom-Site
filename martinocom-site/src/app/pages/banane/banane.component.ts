a0d0ae08512ca25e9d2b2c2304552c1945072145
Marcin Pabich <martinoqom@gmail.com>
2018-11-12 14:12:48 +0100

    Adding new things

--- martinocom-site/src/app/pages/banane/banane.component.ts
+++ martinocom-site/src/app/pages/banane/banane.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Renderer2, } from '@angular/core';
+import * as $ from 'jquery';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-banane',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BananeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private http: HttpClient) {
+    this.renderer.addClass(document.body, 'no-scroll');
+  }
 
-  ngOnInit() {
+  personCount: number = 45;
+  counter: number = 0;
+  text: string = "";
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.renderer.removeClass(document.body, 'no-scroll');
+  }
+
+  onBananaClick() {
+    this.counter+=2;
+
+    $('<div/>', { class: 'chute' }).appendTo('#main');
+    $('<div/>', { class: 'chute small' }).appendTo('#main');
+
+    var box = $('#main');
+    var width = box.width();
+    var height = box.height() + 100;
+    var chute = $('.chute');
+
+    chute.each(function foo() {
+
+      //generate random values
+      var top = (Math.random() * height) | 0;
+      var left = (Math.random() * width) | 0;
+      var time = Math.random() * (800 - 400) + 400 | 0;
+
+      //animate
+      //we introduce a random value so that they aren't moving together
+      //after the animation, we call foo for the current element
+      //to animate the current element again
+      $(this).animate({
+        left: left,
+        top: top
+      }, time, foo);
+    });
+
+    if (this.counter == 1) 
+      this.text = this.counter + " banana!";
+    else if (this.counter < 20)
+      this.text = this.counter + " banane!";
+    else if (this.counter < 40)
+      this.text = "Ti piacciono fin troppo " + this.counter + " banane... eh?";
+    else if (this.counter < 60)
+      this.text = "Non hai meglio da fare che guardare " + this.counter + " banane?";
+    else if (this.counter < 80)
+      this.text = "Ci credi che hai fatto nascere " + this.counter + " banane?";
+    else if (this.counter < 120)
+      this.text = "Mi arrendo: " + this.counter + " banane sono troppe!";
+    else if (this.counter < 140)
+      this.text = "HO DETTO MI ARRENDO! (" + this.counter + ")";
+    else if (this.counter < 110)
+      this.text = "... (" + this.counter + ")";
+    else if (this.counter < 150000)
+      this.text = "Radioattività della pagina: " + (this.counter / 10).toFixed(1) + " micro-sievert";
+    else
+      this.text = "Coraggioso... Hai superato la dose di " + (this.counter / 150000).toFixed(0) + " TAC (" + this.counter +")";
   }
 
 }
